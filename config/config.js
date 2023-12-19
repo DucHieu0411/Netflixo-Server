798f41c6c2dccad8ebac3514ce26c2651aa60a2a
DucHieu0411 <107285882+DucHieu0411@users.noreply.github.com>
2023-12-19 14:54:29 +0700

    complete project

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const vnp_TmnCode = "A1SMVY0W";
+export const vnp_HashSecret = "HDLYVOPLNYIMUFUAJLLBNRUYKQIZODVE";
+export const vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+export const vnp_ReturnUrl = "http://localhost:5000/api/users/vnpay_return";
